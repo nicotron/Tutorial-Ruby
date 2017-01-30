03450460c98789db1099df6d794018efdf17d8f9
joseSantorcuato <santorcuato76@gmail.com>
2017-01-30 17:02:58 -0300

    Actualización 30/01/2017

---
+++ rangos.rb
@@ -0,0 +1,19 @@
+bienvenida = "RANGOS - \nTutorial de Ruby \nJosé Santorcuato Tapia\nChile 2017"
+  puts bienvenida
+
+espacio = " \n"
+  puts espacio
+  rango = "Contar de 1 a 1000: "
+  puts rango
+  puts espacio
+  umbral = (1..1000).to_a
+
+  puts "#{umbral}"
+  puts espacio
+
+  abc = "Nombrar letras de a hasta j: "
+  puts abc
+  puts espacio
+  abcrango = ('a'..'j').to_a
+
+  puts "#{abcrango}"
